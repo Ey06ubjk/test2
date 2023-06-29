efdef2e1aa1c059949c5cc3a26054b11fcf91716
Ey06ubjk <Eyub.cihan@hva.nl>
2023-06-29 13:30:22 +0200

    Update MyTokenBob.sol

--- MyTokenBob.sol
+++ MyTokenBob.sol
@@ -9,7 +9,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract MyTokenBob is ERC20 {
     //when deploying pass in the name and symbol
 
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+    constructor(byte memory name, string memory symbol) ERC20(name, symbol) {
         //mint 100 tokens to msg.sender
         //similar to how 1 dollar = 100 cents
         //1 token = 1 * (10 ** decimals)
